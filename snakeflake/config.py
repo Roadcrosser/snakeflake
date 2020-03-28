4ced9a66295ae1f2802ef968f543faeb6ee10a8a
Roadcrosser <roadcrosser0@gmail.com>
2020-03-28 15:59:07 +0800

    Fix orphaned variable

--- snakeflake/config.py
+++ snakeflake/config.py
@@ -3,6 +3,7 @@
 import datetime
 from snakeflake.utils import get_ip, ipv4_to_int
 
+
 class SnakeflakeConstants:
     """
     The Snakeflake Config class that deals with constants
@@ -12,30 +13,33 @@ class SnakeflakeConstants:
     You can change them to suit your needs if you need to, I suppose.
     """
 
-    def __init__(self, _timestamp_bits, _timescale, _serial_bits, _machine_id_bits):
-        self.timestamp_bits = _timestamp_bits
-        self.timescale = _timescale
-        self.serial_bits = _serial_bits
-        self.machine_id_bits = _machine_id_bits
-
+    def __init__(self, timestamp_bits, timescale, serial_bits, machine_id_bits):
+        self.timestamp_bits = timestamp_bits
+        self.timescale = timescale
+        self.serial_bits = serial_bits
+        self.machine_id_bits = machine_id_bits
 
     @classmethod
     def defaults(cls):
         return cls(39, 10000, 8, 16)
 
 
-
-
 class SnakeflakeGeneratorConfig:
     """The Snakeflake Generator Config class"""
 
-    def __init__(self, epoch:datetime.datetime, machine_id:int=None, constants:SnakeflakeConstants=None, timestamp_method=None):
+    def __init__(
+        self,
+        epoch: datetime.datetime,
+        machine_id: int = None,
+        constants: SnakeflakeConstants = None,
+        timestamp_method=None,
+    ):
         if constants == None:
             constants = SnakeflakeConstants.defaults()
-        
+
         if machine_id == None:
-            machine_id = ipv4_to_int(get_ip(), self._machine_id_bits)
-        
+            machine_id = ipv4_to_int(get_ip(), constants.machine_id_bits)
+
         if timestamp_method == None:
             timestamp_method = datetime.datetime.utcnow
 
